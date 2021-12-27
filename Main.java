341027d8b898980dd922cfdf7fddebe96a7be2c9
akrzysik <95127620+akrzysik@users.noreply.github.com>
2021-12-27 19:17:49 +0000

    maximum i srednia

--- Main.java
+++ Main.java
@@ -1,11 +1,40 @@
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
+
+    public static int minimum(int[] tablica, int rozmiar) {
+      int min = tablica[0];
+      for ( int i = 1; i < rozmiar; i++ )
+         if ( tablica[i] < min )
+            min = tablica[i];
+      return min;
+    }  
+
+    public static int maximum(int[] tablica, int rozmiar) {
+      int max = tablica[0];
+      for ( int i = 1; i < rozmiar; i++ )
+         if ( tablica[i] > max )
+            max = tablica[i];
+      return max;
+    }  
+
+    public static double srednia(int[] tablica, int rozmiar) {
+      return suma(tablica, rozmiar) / (double)rozmiar;
+    }
+    public static int suma(int[] tablica, int rozmiar) {
+      int s = 0;
+      for ( int i = 0; i < rozmiar; i++ )
+         s += tablica[i];
+      return s;
+    }
 
    public static void main(String[] args) {
       try {
          BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
          int x, y;
+         Random rand = new Random();
          System.out.println("Podaj liczbe wierszy macierzy: ");
          x = Integer.parseInt(in.readLine());
          System.out.println("Podaj liczbe kolumn macierzy: ");
@@ -14,8 +43,11 @@ public class Main {
          
          for ( int i = 0; i < x; i++ ) {
             for ( int j = 0; j < y; j++ ) {
-               System.out.println("Podaj element ["+i+"]["+j+"]: ");
-               m[i][j] = Integer.parseInt(in.readLine());
+              //System.out.println("Podaj element ["+i+"]["+j+"]: ");
+              // m[i][j] = Integer.parseInt(in.readLine());
+              m[i][j] = rand.nextInt(100);
+              System.out.println("Element ["+i+"]["+j+"]: " + m[i][j]);
+
             }
          }
          
@@ -30,6 +62,15 @@ public class Main {
          System.out.println("Najmniejszy element znajduje sie w wierszu nr "+
                             (min_x)+" i kolumnie nr "+(min_y)+
                             " a jego wartosc wynosi "+m[min_x][min_y]);
+
+         int[] minima = new int[x];
+         for (int i=0; i<x; i++) {
+           minima[i]=minimum(m[i], y);
+           System.out.println("Minimum wiersza "+i+ " " + minima[i]);
+         }
+
+         System.out.println("Maximum z minimów " + maximum(minima, x));
+         System.out.println("Średnia z minimów " + srednia(minima, x));
       } catch(IOException e) {
       }
    }
